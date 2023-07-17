7f44672bc1dabcbddf33f5fbd5f63f07ab49e6e6
Slash <wwwroot@mail.ksu.edu.tw>
2023-07-17 09:42:21 +0800

    Init

---
+++ Project_SlashviewArchive/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Project_SlashviewArchive
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("This project is for sharing internet archives.");
+      Console.Read();
+    }
+  }
+}
